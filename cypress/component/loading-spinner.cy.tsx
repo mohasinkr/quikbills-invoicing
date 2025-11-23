bf256712ea46aac6bda95a30a7f1b56946c2649b
Mohasin K R <mohasin61@gmail.com>
2025-11-23 23:34:58 +0530

    feat: update the cypress test

--- cypress/component/loading-spinner.cy.tsx
+++ cypress/component/loading-spinner.cy.tsx
@@ -5,11 +5,9 @@ describe("<LoadingSpinner />", () => {
   it("renders", () => {
     // see: https://on.cypress.io/mounting-react
     cy.mount(<LoadingSpinner spinnerClassName="text-black" />);
-  }),
-    it("should have the text 'Loading'", () => {
-      cy.mount(
-        <LoadingSpinner spinnerClassName="text-black" label="Loading" />
-      );
-      cy.get("[data-cy=loading-text]").should("have.text", "Loading");
-    });
+  });
+  it("should have the text 'Loading'", () => {
+    cy.mount(<LoadingSpinner spinnerClassName="text-black" label="Loading" />);
+    cy.get("[data-cy=loading-text]").should("have.text", "Loading");
+  });
 });
